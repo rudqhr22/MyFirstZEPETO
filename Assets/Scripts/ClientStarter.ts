548fb259660437bcf1308c9644055069c598403b
rudqhr22 <rudqhr22@naver.com>
2022-06-16 15:18:02 +0900

    server code 02

--- Assets/Scripts/ClientStarter.ts
+++ Assets/Scripts/ClientStarter.ts
@@ -1,82 +1,151 @@
-import { CharacterState, SpawnInfo, ZepetoPlayers } from 'ZEPETO.Character.Controller';
-import { Room, RoomData } from 'ZEPETO.Multiplay';
-import *  as UnityEngine from 'UnityEngine';
-import { Player, State, Vector } from 'ZEPETO.Multiplay.Schema';
-import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
-import { ZepetoWorldMultiplay } from 'ZEPETO.World'
+import {ZepetoScriptBehaviour} from 'ZEPETO.Script'
+import {ZepetoWorldMultiplay} from 'ZEPETO.World'
+import {Room, RoomData} from 'ZEPETO.Multiplay'
+import {Player, State, Vector} from 'ZEPETO.Multiplay.Schema'
+import {CharacterState, SpawnInfo, ZepetoPlayers, ZepetoPlayer} from 'ZEPETO.Character.Controller'
+import * as UnityEngine from "UnityEngine";
 
-export default class ClientStarter extends ZepetoScriptBehaviour {
 
-    public multiPlay : ZepetoWorldMultiplay;
-    private  room : Room;
+export default class Starter extends ZepetoScriptBehaviour {
 
-    private  currentPlayer: Map<string, Player> = new Map<string, Player>()
+    public multiplay: ZepetoWorldMultiplay;
 
-    Start()
-    {
-       this.multiPlay.RoomCreated += (_room :Room) =>
-       {
-            this.room = _room;
-       };
+    private room: Room;
+    private currentPlayers: Map<string, Player> = new Map<string, Player>();
 
-       this.multiPlay.RoomJoined += (_room :Room) =>
-       {
-            _room.OnStateChange += this.onStateChange;
-       };
+    Start() {
+
+        this.multiplay.RoomCreated += (room: Room) => {
+            this.room = room;
+        };
+
+        this.multiplay.RoomJoined += (room: Room) => {
+            room.OnStateChange += this.OnStateChange;
+        };
+
+        this.StartCoroutine(this.SendMessageLoop(0.1));
     }
 
+    // Send the local character transform to the server at the scheduled Interval Time.
+    private* SendMessageLoop(tick: number) {
+        while (true) {
+            yield new UnityEngine.WaitForSeconds(tick);
 
-    private onStateChange(state  :State, isFirst : boolean)
-    {
-        console.log("onStateChange");
+            if (this.room != null && this.room.IsConnected) {
+                const hasPlayer = ZepetoPlayers.instance.HasPlayer(this.room.SessionId);
+                if (hasPlayer) {
+                    const myPlayer = ZepetoPlayers.instance.GetPlayer(this.room.SessionId);
+                    if (myPlayer.character.CurrentState != CharacterState.Idle)
+                        this.SendTransform(myPlayer.character.transform);
+                }
+            }
+        }
+    }
 
-        if(isFirst)
-        {
-            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(()=>{
+    private OnStateChange(state: State, isFirst: boolean) {
+
+        // When the first OnStateChange event is received, a state full snapshot is received.
+        if (isFirst) {
+
+            // [CharacterController] (Local) Called when the Player instance is fully loaded in Scene
+            ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
                 const myPlayer = ZepetoPlayers.instance.LocalPlayer.zepetoPlayer;
 
                 myPlayer.character.OnChangedState.AddListener((cur, prev) => {
                     this.SendState(cur);
-
                 });
+            });
+
+            // [CharacterController] Called when the Player instance is fully loaded in Scene
+            ZepetoPlayers.instance.OnAddedPlayer.AddListener((sessionId: string) => {
+                const isLocal = this.room.SessionId === sessionId;
+                if (!isLocal) {
+                    const player: Player = this.currentPlayers.get(sessionId);
+
+                    // Called whenever the state of the [RoomState] player instance is updated.
+                    player.OnChange += (changeValues) => this.OnUpdatePlayer(sessionId, player);
+                }
             });
         }
 
         let join = new Map<string, Player>();
-        state.players.ForEach((sessionId: string, player:Player) => {
-            if(!this.currentPlayer.has(sessionId))
-            {
-                join.set(sessionId, player);
+        let leave = new Map<string, Player>(this.currentPlayers);
 
+        state.players.ForEach((sessionId: string, player: Player) => {
+            if (!this.currentPlayers.has(sessionId)) {
+                join.set(sessionId, player);
             }
+            leave.delete(sessionId);
         });
 
-        join.forEach((player:Player, sessionId: string) => this.OnJoinPlayer(player, sessionId));
+        // [RoomState] Create a player instance that entered the Room
+        join.forEach((player: Player, sessionId: string) => this.OnJoinPlayer(sessionId, player));
 
+        // [RoomState] Remove exited player instance from Room
+        leave.forEach((player: Player, sessionId: string) => this.OnLeavePlayer(sessionId, player));
     }
 
-    private SendState(state : CharacterState)
-    {
-        const data = new RoomData();
-        data.Add("state", state);
-        this.room.Send("onChangedState", data.GetObject)
-    }
-
-    private OnJoinPlayer(player:Player, sessionId: string)
-    {
-        console.log("OnJoinPlayer :" + player + "sessionId : " + sessionId);
-        this.currentPlayer.set(sessionId, player);
+    private OnJoinPlayer(sessionId: string, player: Player) {
+        console.log(`[OnJoinPlayer] players - sessionId : ${sessionId}`);
+        this.currentPlayers.set(sessionId, player);
 
         const spawnInfo = new SpawnInfo();
-        const pos = new UnityEngine.Vector3(0,0,0);
-        const rot = new UnityEngine.Vector3(0,0,0);
-
-        spawnInfo.position = pos;
-        spawnInfo.rotation = UnityEngine.Quaternion.Euler(rot);
-
-
+        const position = this.ParseVector3(player.transform.pos);
+        const rotation = this.ParseVector3(player.transform.rot);
+        spawnInfo.position = position;
+        spawnInfo.rotation = UnityEngine.Quaternion.Euler(rotation);
 
         const isLocal = this.room.SessionId === player.id;
-        ZepetoPlayers.instance.CreatePlayerWithUserId(sessionId, player.id, spawnInfo, isLocal);
+        ZepetoPlayers.instance.CreatePlayerWithUserId(sessionId, player.userId, spawnInfo, isLocal);
+    }
+
+    private OnLeavePlayer(sessionId: string, player: Player) {
+        console.log(`[OnRemove] players - sessionId : ${sessionId}`);
+        this.currentPlayers.delete(sessionId);
+
+        ZepetoPlayers.instance.RemovePlayer(sessionId);
+    }
+
+    private OnUpdatePlayer(sessionId: string, player: Player) {
+
+        const position = this.ParseVector3(player.transform.pos);
+
+        const zepetoPlayer = ZepetoPlayers.instance.GetPlayer(sessionId);
+        zepetoPlayer.character.MoveToPosition(position);
+
+        if (player.state === CharacterState.JumpIdle || player.state === CharacterState.JumpMove)
+            zepetoPlayer.character.Jump();
+    }
+
+    private SendTransform(transform: UnityEngine.Transform) {
+        const data = new RoomData();
+
+        const pos = new RoomData();
+        pos.Add("x", transform.localPosition.x);
+        pos.Add("y", transform.localPosition.y);
+        pos.Add("z", transform.localPosition.z);
+        data.Add("position", pos.GetObject());
+
+        const rot = new RoomData();
+        rot.Add("x", transform.localEulerAngles.x);
+        rot.Add("y", transform.localEulerAngles.y);
+        rot.Add("z", transform.localEulerAngles.z);
+        data.Add("rotation", rot.GetObject());
+        this.room.Send("onChangedTransform", data.GetObject());
+    }
+
+    private SendState(state: CharacterState) {
+        const data = new RoomData();
+        data.Add("state", state);
+        this.room.Send("onChangedState", data.GetObject());
+    }
+
+    private ParseVector3(vector3: Vector): UnityEngine.Vector3 {
+        return new UnityEngine.Vector3
+        (
+            vector3.x,
+            vector3.y,
+            vector3.z
+        );
     }
 }
--- Assets/World.multiplay/index.ts
+++ Assets/World.multiplay/index.ts
@@ -1,6 +1,6 @@
 import { Sandbox, SandboxOptions, SandboxPlayer } from "ZEPETO.Multiplay";
 import { DataStorage } from "ZEPETO.Multiplay.DataStorage";
-import { Player } from "ZEPETO.Multiplay.Schema";
+import { Player, Transform, Vector } from "ZEPETO.Multiplay.Schema";
 
 export default class extends Sandbox
 {
@@ -17,50 +17,68 @@ export default class extends Sandbox
 
     onCreate(options: SandboxOptions)
     {
+        this.onMessage("onChangedTransform", (client, message) => {
+            const player = this.state.players.get(client.sessionId);
+
+            const transform = new Transform();
+            transform.pos = new Vector();
+            transform.pos.x = message.position.x;
+            transform.pos.y = message.position.x;
+            transform.pos.z = message.position.x;
+
+            transform.rot = new Vector();
+            transform.rot.x = message.rotation.x;
+            transform.rot.y = message.rotation.y;
+            transform.rot.z = message.rotation.z;
+
+            player.transform = transform;
+        });
+
         this.onMessage("onChangedState", (client, message) => {
             const player = this.state.players.get(client.sessionId);
             player.state = message.state;
         });
     }
 
-    onJoin(client: SandboxPlayer)
+    async onJoin(client: SandboxPlayer)
     {
-        console.log("join");
-        //this.player = new Player();
-        let player = new Player();
+        const player = new Player();
+        player.id = client.sessionId;
 
-        player.id = client.userId;
-        player.hash = client.hashCode;
-        player.userId = client.userId;
+        if (client.hashCode) {
+            player.hash = client.hashCode;
+        }
+        if (client.userId) {
+            player.userId = client.userId;
+        }
 
 
-        //this.setPlayer(client);
-        this.setStorage(client);
-        this.state.players.set(client.sessionId, player);
-    }
 
-    onLeave(client: SandboxPlayer, consented?: boolean)
-    {
-        console.log("leave");
+        const transform = new Transform();
+        transform.pos.x = this.state.players.size;
+        transform.pos.y = 0;
+        transform.pos.z = 0;
 
-    }
+        transform.rot = new Vector();
+        transform.rot.x = 0;
+        transform.rot.y = 0;
+        transform.rot.z = 0;
 
-    setPlayer(_ : SandboxPlayer)
-    {
-        //this.player.id = _.userId;
-       // this.player.hash = _.hashCode;
-       // this.player.userId = _.userId;
-    }
+        player.transform = transform;
 
-    async setStorage(_ : SandboxPlayer)
-    {
-        //= client
-        //this.storage
-        const storage = _.loadDataStorage();
+
+        const storage = client.loadDataStorage();
 
         let visitCnt = await storage.get("VisitCount") as number;
         if(visitCnt == null) visitCnt = 0;
 
         await storage.set("VisitCount" , ++visitCnt);
+
+        this.state.players.set(client.sessionId, player);
+    }
+
+    onLeave(client: SandboxPlayer, consented?: boolean)
+    {
+        this.state.players.delete(client.sessionId);
     }
 }
